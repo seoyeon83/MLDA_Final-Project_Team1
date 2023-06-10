5f67afa9a3ded6ef7bf8597175f63a9344ab6338
Ahyoung Cho <joayoung03@gmail.com>
2023-06-10 14:35:10 +0900

    [2023.06.10] 어리를빗 수정

--- code/review_pre/review_concat_ay.py
+++ code/review_pre/review_concat_ay.py
@@ -7,8 +7,10 @@ def concat_review(review_list, musical_name):
 
     # 데이터 가져와서 합치기
     for i in review_list:
-        re1 = pd.read_csv(f"C:/Users/1ayou/PycharmProjects/MLDA_Final-Project_Team1/data/reviews/{i}.csv")
+        print(i)
+        re1 = pd.read_csv(f"../MLDA_Final-Project_Team1/data/reviews/{i}.csv")
         cc_review = pd.concat([cc_review, re1], axis=0, join='outer', ignore_index=True)
+
 
     # 조회수, 공감수 컬럼 삭제, 이상한 컬럼 삭제
     # cc_review.drop(columns=["views", "like"], axis=1, inplace=True)
@@ -16,14 +18,13 @@ def concat_review(review_list, musical_name):
     cc_review = cc_review.reset_index().rename(columns={"":"Index", "id":"Rating", "date":"Id", "rating":"Date", "title":"Title", "text":"Text"})
 
     # csv 파일로 저장
-    cc_review.to_csv(f"C:/Users/1ayou/PycharmProjects/MLDA_Final-Project_Team1/data/tot_review/review_{musical_name}.csv", index=True, sep=",", encoding="utf-8-sig")
+    cc_review.to_csv(f"../MLDA_Final-Project_Team1/data/tot_review/review_{musical_name}.csv", index=False, sep=",", encoding="utf-8-sig")
     # 확인용 return
     return print(cc_review.value_counts().sum())
 
 # 흠.. 이걸 노가다로 이렇게 안할 수 있는 방법이 있을까?
 # 마땅이 무언가 떠오르지 않아 노가다로 해둠...
-musical = ["그리스", "브로드웨이42번가", "빌리엘리어트", "헤드윅", "호프", "맘마미아", "렌트",
-           "젠틀맨스가이드", "썸씽로튼", "벤허", "팬레터", "마리퀴리" "레드북", "킹아더", "렛미플라이"]
+
 grease = ["review_그리스_26", "review_그리스_27", "review_그리스_28"]
 broad = ["review_브로드웨이42번가_18", "review_브로드웨이42번가_17",
          "review_브로드웨이42번가_16", "review_브로드웨이42번가_15"]
@@ -43,6 +44,9 @@ let = ["review_렛미플라이_1"]
 
 pre = [grease, broad, billie, head, hope, mamma, rent,
        gentle, some, ben, pan, mari, red, king, let]
+
+musical = ["그리스", "브로드웨이42번가", "빌리엘리어트", "헤드윅", "호프", "맘마미아", "렌트",
+           "젠틀맨스가이드", "썸씽로튼", "벤허", "팬레터", "마리퀴리", "레드북", "킹아더", "렛미플라이"]
 
 # 하나씩 꺼내서 함수 돌리기
 for i in range(len(musical)):
