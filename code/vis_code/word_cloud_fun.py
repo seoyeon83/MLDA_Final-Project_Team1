df07e534ddcc89e166b8065f021c9f44de4f8aea
seoyeon83 <mool848777@naver.com>
2023-06-11 17:10:46 +0900

    [23.06.11] 불용어 처리 안되던 문제 해결_서연

--- code/vis_code/word_cloud_fun.py
+++ code/vis_code/word_cloud_fun.py
@@ -5,11 +5,14 @@ import pandas as pd
 
 
 def make_cloud():
-    data_dir = ("C:/Users/1ayou/PycharmProjects/MLDA_Final-Project_Team1/data/morph_data_okt")
+    # 상대경로로 수정함
+    data_dir = ("../../data/morph_data_okt/spacing이전")
     file_list = os.listdir(data_dir)
 
-    with open("new_stopwords.txt", "r", encoding="utf-8") as f:
-        stopwords = f.readlines()  # list
+    with open("newnew_stopwords.txt", "r", encoding="utf-8") as f:
+        stopwords_list = f.readlines()  # list
+
+    stopwords = stopwords_list[0].split(",")
 
     for i in file_list:
         data = pd.read_csv(f"{data_dir}/{i}")
@@ -17,11 +20,11 @@ def make_cloud():
         print(i)
 
         for j in data.iloc[:, 0]:
-            if j not in stopwords:
+            if j not in stopwords and len(j) > 1: # 단어 크기가 2 이상인 것만
                 musical_morphs.append(j)
 
         # 잘 들어갔나 확인
-        # print(musical_morphs)
+        print(musical_morphs)
 
         # 빈도수 카운트 후 가장 많이 나온 단어부터 100개
         counts = Counter(musical_morphs)
@@ -31,7 +34,8 @@ def make_cloud():
                        width=1000, height=1000, max_font_size=300)
         cloud = wc.generate_from_frequencies(dict(tags))
 
-        cloud.to_file(f"C:/Users/1ayou/PycharmProjects/MLDA_Final-Project_Team1/visualization/word_cloud/{i[:-4]}.jpg")
+        # 상대경로로 수정
+        cloud.to_file(f"../../visualization/word_cloud/spacing이전/{i[:-4]}.jpg")
 
 
 make_cloud()
