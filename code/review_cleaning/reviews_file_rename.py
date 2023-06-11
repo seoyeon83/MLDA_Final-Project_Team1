0834e39923c50239100b45df27a666c403a74322
seoyeon83 <mool848777@naver.com>
2023-06-12 00:32:26 +0900

    [23.06.12] 파일명 통일_서연

---
+++ code/review_cleaning/reviews_file_rename.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+
+file_path  = ("../../data/morph_data_okt")
+
+file_list = os.listdir(file_path)
+
+for i in file_list[2:]:
+    src = os.path.join(file_path, i)
+    print(f'i : {i}')
+    if "_spacing" in i :
+        dst = ''.join(i.split('_spacing'))
+
+    elif "reviews" not in i :
+        if "adj" in i :
+            dst = f'{i[:3]}_reviews{i[3:]}'
+        elif "noun" in i :
+            dst = f'{i[:4]}_reviews{i[4:]}'
+
+    print(f'dst {dst}')
+    dst = os.path.join(file_path, dst)
+    os.rename(src, dst)
+
+
+    # data = pd.read_csv(f"{data_dir}/{i}")
